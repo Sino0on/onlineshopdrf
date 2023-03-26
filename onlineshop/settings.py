8f551f993b668b3bfb1ec398084a4fa11d3b7b30
Sino0on <dastan.kubanuulu1@gmail.com>
2023-03-26 18:53:20 +0600

    das

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -131,9 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
+
+STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR / "media"
 
