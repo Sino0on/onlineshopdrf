a40b4e2a474d6abe40d4d66918e33daa6d1f5abc
Sino0on <dastan.kubanuulu1@gmail.com>
2023-03-26 18:49:26 +0600

    das

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api_schema', get_schema_view(title='API Schema', description='Guide for drf'), name='api_schema'),
@@ -26,4 +28,4 @@ urlpatterns = [
         template_name='docs.html',
         extra_context={'schema_url': 'openapi-schema'}
     ), name='swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
