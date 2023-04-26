51d6bde34c4d593a66fca52c262cb9cfeccee8f3
Sino0on <dastan.kubanuulu1@gmail.com>
2023-04-26 13:39:47 +0600

    das

--- server/filters.py
+++ server/filters.py
@@ -2,13 +2,14 @@ import django_filters
 from .models import *
 
 
-class ProductFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
+class ProductFilter(django_filters.FilterSet, django_filters.OrderingFilter):
+    title = django_filters.CharFilter(lookup_expr='istartswith')
     price = django_filters.NumberFilter()
     price_from = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
     price__to = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    ordering = django_filters.OrderingFilter(fields=('price', 'likes'))
 
     class Meta:
         model = Product
-        fields = ['title', 'types', 'size', 'category', 'price']
+        fields = ['title', 'types', 'size', 'category', 'price', 'ordering']
 
