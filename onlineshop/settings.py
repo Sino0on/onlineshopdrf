fc86a2bbf5cf0b43fa16020d20f4e124142cff76
Sino0on <dastan.kubanuulu1@gmail.com>
2023-04-27 14:21:16 +0600

    das

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -87,6 +87,8 @@ AUTH_USER_MODEL = 'server.User'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
